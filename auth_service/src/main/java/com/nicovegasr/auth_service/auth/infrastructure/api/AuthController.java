60a679aa9a1b708bbb18b128b14259eb227b899b
nicovegasr <nvegas01@gmail.com>
2024-04-18 20:00:07 +0100

    (auth controller) - First implementation of auth controller

--- auth_service/src/main/java/com/nicovegasr/auth_service/auth/infrastructure/api/AuthController.java
+++ auth_service/src/main/java/com/nicovegasr/auth_service/auth/infrastructure/api/AuthController.java
@@ -8,10 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.nicovegasr.auth_service.auth.infrastructure.api.requests.CredentialsRequest;
 import com.nicovegasr.auth_service.auth.infrastructure.api.responses.LoginResponse;
+import com.nicovegasr.auth_service.auth.infrastructure.repositories.user_repositories.UserJpaToDomainRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+    private final UserJpaToDomainRepository userRepository;
 
     @PostMapping("/login/credentials")
     ResponseEntity<LoginResponse> loginWithCredentials(@RequestBody CredentialsRequest credentials) {
