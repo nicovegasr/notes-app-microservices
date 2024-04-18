3cea27986acd759871604f8ff54ef1a48e5e3cd3
nicovegasr <nvegas01@gmail.com>
2024-04-18 20:51:29 +0100

    (test register user) - first implementation of register user usecase test.

---
+++ auth_service/src/test/java/com/nicovegasr/auth_service/integration_test/RegisterUserTest.java
@@ -0,0 +1,16 @@
+package com.nicovegasr.auth_service.integration_test;
+
+import org.junit.jupiter.api.Test;
+
+public class RegisterUserTest {
+    /**
+     * 1.- User with vaild credentials (username & password) should be created.
+     * 2.- User that already exist should not be created.
+     * 3.- User with invalid username should not be created.
+     * 4.- User with invalid password should not be created.
+     */
+    @Test
+    void userWithValidCredentialsShouldBeCreated() {
+    
+    }
+}
