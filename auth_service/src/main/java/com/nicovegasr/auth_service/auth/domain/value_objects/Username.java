28bf195ceab3c88e695e604ab88449b2206cff5c
nicovegasr <nvegas01@gmail.com>
2024-04-20 11:40:51 +0100

    (test - red) Username value object test

--- auth_service/src/main/java/com/nicovegasr/auth_service/auth/domain/value_objects/Username.java
+++ auth_service/src/main/java/com/nicovegasr/auth_service/auth/domain/value_objects/Username.java
@@ -1,5 +1,14 @@
 package com.nicovegasr.auth_service.auth.domain.value_objects;
 
-public class Username {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@AllArgsConstructor
+@Getter
+public class Username {
+    String name;
+
+    public static Username build(String name) {
+        return new Username(name);
+    }
 }
--- auth_service/src/test/java/com/nicovegasr/auth_service/unit_test/models/UsernameTest.java
+++ auth_service/src/test/java/com/nicovegasr/auth_service/unit_test/models/UsernameTest.java
@@ -1,6 +1,23 @@
 package com.nicovegasr.auth_service.unit_test.models;
 
-public class UsernameTest {
-        /** Use cases:
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import com.nicovegasr.auth_service.auth.domain.value_objects.Username;
+
+class UsernameTest {
+    /**
+     * Use cases:
+     * 1.- Username with empty string should throw an exception
+     * 2.- Username with null value should throw an exception
+     * 3.- Username with name length less than 4 should throw an exception
      */
+    @Test
+    void testUsernameWithEmptyString() {
+        Username username = Username.build("");
+        assertThrows(IllegalArgumentException.class, () -> {
+            username.getName();
+        });
+    }
 }
