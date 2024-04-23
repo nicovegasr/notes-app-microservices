3652c788e82140ea7943fd85fbc90bbcebe5b3cd
nicovegasr <nvegas01@gmail.com>
2024-04-23 20:10:12 +0100

    (test ✅ ) -  User tests and feature implemented

--- auth_service/src/main/java/com/nicovegasr/auth_service/auth/domain/models/User.java
+++ auth_service/src/main/java/com/nicovegasr/auth_service/auth/domain/models/User.java
@@ -2,17 +2,30 @@ package com.nicovegasr.auth_service.auth.domain.models;
 
 import java.time.LocalDate;
 
-import lombok.Builder;
-import lombok.Data;
+import com.nicovegasr.auth_service.auth.domain.value_objects.Password;
+import com.nicovegasr.auth_service.auth.domain.value_objects.Username;
 
-@Data
-@Builder
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class User {
-    String username;
-
-    String password;
-
+    Username username;
+    Password password;
     LocalDate createdAt;
-
     LocalDate lastLoginDate;
+
+    public static User create(
+            String username,
+            String password,
+            LocalDate createdAt,
+            LocalDate lastLoginDate) {
+        return new User(
+                Username.create(username),
+                Password.create(password),
+                createdAt,
+                lastLoginDate);
+    }
 }
--- auth_service/src/test/java/com/nicovegasr/auth_service/unit_test/models/UserTest.java
+++ auth_service/src/test/java/com/nicovegasr/auth_service/unit_test/models/UserTest.java
@@ -1,6 +1,45 @@
 package com.nicovegasr.auth_service.unit_test.models;
 
-public class UserTest {
-    /** Use cases:
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+import com.nicovegasr.auth_service.auth.domain.models.User;
+
+class UserTest {
+    /**
+     * Use cases:
+     * 1. User without valid username should throw exception
+     * 2. User without valid password should throw exception
+     * 3. Create a user with a valid username and password
      */
+
+    @Test
+    void userWithoutValidUsernameShouldThrowException() {
+        LocalDate now = LocalDate.now();
+        assertThrows(IllegalArgumentException.class, () -> {
+            User.create("tes", "TestPassword1+", now, now);
+        });
+    }
+
+    @Test
+    void userWithoutValidPasswordShouldThrowException() {
+        LocalDate now = LocalDate.now();
+        assertThrows(IllegalArgumentException.class, () -> {
+            User.create("test", "test", now, now);
+        });
+    }
+
+    @Test
+    void createUserWithValidUsernameAndPassword() {
+        LocalDate now = LocalDate.now();
+        User user = User.create("test", "TestPassword1+", now, now);
+
+        assertEquals("test", user.getUsername().getName());
+        assertEquals("TestPassword1+", user.getPassword().getCredential());
+    }
+
 }
