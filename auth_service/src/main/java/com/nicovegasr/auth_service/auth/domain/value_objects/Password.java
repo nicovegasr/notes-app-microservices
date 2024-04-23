50bd79d88f63d72bb4703a8a1d5a40f11af8439a
nicovegasr <nvegas01@gmail.com>
2024-04-23 18:35:12 +0100

    (test ✅ ) -  Password VO tests and feature implemented

--- auth_service/src/main/java/com/nicovegasr/auth_service/auth/domain/value_objects/Password.java
+++ auth_service/src/main/java/com/nicovegasr/auth_service/auth/domain/value_objects/Password.java
@@ -1,5 +1,30 @@
 package com.nicovegasr.auth_service.auth.domain.value_objects;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class Password {
-    
+    String credential;
+
+    public static Password create(String password) {
+        if (password == null || password.isEmpty()) {
+            throw new IllegalArgumentException("Password cannot be null or empty");
+        }
+        if (password.length() < 8) {
+            throw new IllegalArgumentException("Password length should be at least 8 characters");
+        }
+        if (!password.matches(
+                "^(?=.*[\\d])"      +
+                "(?=.*[a-z])"       + 
+                "(?=.*[A-Z])"       +
+                "(?=.*[@#$%^&+=])"  + 
+                "(?=\\S+$).{8,}$")
+            ) {
+            throw new IllegalArgumentException(
+                    "Password should contain at least one digit, one lowercase letter, one uppercase letter, one special character and no whitespace");
+        }
+        return new Password(password);
+    }
 }
--- auth_service/src/test/java/com/nicovegasr/auth_service/unit_test/models/PasswordTest.java
+++ auth_service/src/test/java/com/nicovegasr/auth_service/unit_test/models/PasswordTest.java
@@ -1,6 +1,77 @@
 package com.nicovegasr.auth_service.unit_test.models;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import com.nicovegasr.auth_service.auth.domain.value_objects.Password;
+
 class PasswordTest {
-    /** Use cases:
+    /**
+     * Use cases:
+     * 1.- Password with empty string should throw an exception.
+     * 2.- Password with null value should throw an exception.
+     * 3.- Password with length less than 8 should throw an exception.
+     * 4.- Password without correct format should throw an exception.
+     * 5.- Password with valid length should be created.
      */
+
+    @Test
+    void testPasswordWithEmptyString() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Password.create("");
+        });
+    }
+
+    @Test
+    void testPasswordWithNullValue() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Password.create(null);
+        });
+    }
+
+    @Test
+    void testPasswordWithLengthLessThan8() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Password.create("test");
+        });
+    }
+
+    @Test
+    void testPasswordWithoutMayusShouldNotBeCreated() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Password.create("this_is_a_test_1+");
+        });
+    }
+
+    @Test
+    void testPasswordWithoutDigitShouldNotBeCreated() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Password.create("this_is_a_test_T+");
+        });
+    }
+
+    @Test
+    void testPasswordWithoutSpecialCharacterShouldNotBeCreated() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Password.create("thisIsATest1");
+        });
+    }
+
+    @Test
+    void testPasswordWithBlankSpacesShouldNotBeCreated() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Password.create("thisIs ATest1+");
+        });
+    }
+
+    @Test
+    void testPasswordWithCorrectFormatShouldBeCreated() {
+        String passwordToCreate = "thisIsATest1+";
+
+        Password password = Password.create(passwordToCreate);
+
+        assertEquals(password.getCredential(), passwordToCreate);
+    }
 }
