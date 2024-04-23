4a900e0fbdaec00817e67162116c964b9ac40740
nicovegasr <nvegas01@gmail.com>
2024-04-23 18:54:22 +0100

    (test ♻️ ) -  Password VO refactor

--- auth_service/src/main/java/com/nicovegasr/auth_service/auth/domain/value_objects/Password.java
+++ auth_service/src/main/java/com/nicovegasr/auth_service/auth/domain/value_objects/Password.java
@@ -9,22 +9,31 @@ public class Password {
     String credential;
 
     public static Password create(String password) {
+        validatePassword(password);
+        return new Password(password);
+    }
+
+    private static void validatePassword(String password) {
+        checkIsNullOrEmpty(password);
+        checkIsCorrectFormat(password);
+    }
+
+    private static void checkIsCorrectFormat(String password) {
+        if (!password.matches(
+                "^(?=.*[\\d])" +
+                        "(?=.*[a-z])" +
+                        "(?=.*[A-Z])" +
+                        "(?=.*[@#$%^&+=])" +
+                        "(?=\\S+$).{8,}$")) {
+            throw new IllegalArgumentException(
+                    "Password should contain at least one digit, one lowercase letter, one uppercase letter, one special character no whitespace and length should be at least 8 characters");
+        }
+    }
+
+    private static void checkIsNullOrEmpty(String password) {
         if (password == null || password.isEmpty()) {
             throw new IllegalArgumentException("Password cannot be null or empty");
         }
-        if (password.length() < 8) {
-            throw new IllegalArgumentException("Password length should be at least 8 characters");
-        }
-        if (!password.matches(
-                "^(?=.*[\\d])"      +
-                "(?=.*[a-z])"       + 
-                "(?=.*[A-Z])"       +
-                "(?=.*[@#$%^&+=])"  + 
-                "(?=\\S+$).{8,}$")
-            ) {
-            throw new IllegalArgumentException(
-                    "Password should contain at least one digit, one lowercase letter, one uppercase letter, one special character and no whitespace");
-        }
-        return new Password(password);
     }
+
 }
