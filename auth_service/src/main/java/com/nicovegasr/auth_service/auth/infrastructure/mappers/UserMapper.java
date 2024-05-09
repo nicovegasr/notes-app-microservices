a68a0b33cc42ed5817dc3bf9dd7a86184e182a56
nicovegasr <nvegas01@gmail.com>
2024-05-09 17:34:01 +0100

    Modified mapper to encrypt & decryot password

--- auth_service/src/main/java/com/nicovegasr/auth_service/auth/infrastructure/mappers/UserMapper.java
+++ auth_service/src/main/java/com/nicovegasr/auth_service/auth/infrastructure/mappers/UserMapper.java
@@ -1,5 +1,9 @@
 package com.nicovegasr.auth_service.auth.infrastructure.mappers;
 
+import org.springframework.beans.factory.annotation.Value;
+
+import com.nicovegasr.auth_service.auth.application.usecases.DecryptPassword;
+import com.nicovegasr.auth_service.auth.application.usecases.EncryptPassword;
 import com.nicovegasr.auth_service.auth.domain.models.User;
 import com.nicovegasr.auth_service.auth.infrastructure.entities.UserEntity;
 
@@ -9,18 +13,28 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserMapper {
 
+    @Value("${cipher.algorithm}")
+    private static String cipherAlgorithm;
+
+    @Value("${secret.key}")
+    private static String secretKey;
+
     public static User toDomainModel(UserEntity userEntity) {
+        String decryptedPassword = DecryptPassword.decrypt(userEntity.getPassword(), secretKey, cipherAlgorithm);
         return User.create(
                 userEntity.getUsername(),
-                userEntity.getPassword(),
+                decryptedPassword,
                 userEntity.getCreatedAt(),
                 userEntity.getLastLoginDate());
     }
 
     public static UserEntity toEntity(User user) {
+        String encryptedPassword = EncryptPassword.encrypt(user.getPassword().getCredential(),
+                secretKey,
+                cipherAlgorithm);
         return UserEntity.builder()
                 .username(user.getUsername().getName())
-                .password(user.getPassword().getCredential())
+                .password(encryptedPassword)
                 .createdAt(user.getCreatedAt())
                 .lastLoginDate(user.getLastLoginDate())
                 .build();
