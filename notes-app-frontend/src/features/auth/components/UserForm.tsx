aba80d6474e5c79210bd8cfec53cca7592864040
nicovegasr <nicovegasr@gmail.com>
2024-06-09 22:16:24 +0100

    Login and register form

---
+++ notes-app-frontend/src/features/auth/components/UserForm.tsx
@@ -0,0 +1,49 @@
+import { Button, Input } from "@nextui-org/react";
+import { ChangeEvent, useState } from "react";
+import { User } from "../../../models/User";
+
+interface UserFormProps {
+    mode: string;
+    onSend: (user: User) => void;
+}
+
+export const UserForm = ({ mode, onSend }: UserFormProps) => {
+    const [user, setUser] = useState<User>({ username: '', password: '' });
+
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setUser({ ...user, [name]: value });
+    };
+
+    return (
+        <>
+            <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+                <Input
+                    name="username"
+                    label="Username"
+                    variant="underlined"
+                    placeholder="Username"
+                    fullWidth
+                    onChange={handleInputChange}
+                />
+            </div>
+            <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+                <Input
+                    name="password"
+                    label="Password"
+                    variant="underlined"
+                    placeholder="Password"
+                    fullWidth
+                    type="password"
+                    onChange={handleInputChange}
+                />
+            </div>
+            <Button
+                className="w-full py-2 bg-blue-500 text-white font-bold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-300"
+                onClick={() => onSend(user)}
+            >
+                {mode === 'login' ? 'Login' : 'Register'}
+            </Button>
+        </>
+    )
+}
---
+++ notes-app-frontend/src/features/auth/pages/Register.tsx
@@ -0,0 +1,36 @@
+import {Image, Link} from "@nextui-org/react";
+import {User} from "../../../models/User";
+import AuthRepository from "../../../repositories/AuthRepository";
+import {UserForm} from "../components/UserForm";
+import {useToast} from "../../commons/hooks/useToasts";
+
+export const Register = () => {
+
+    const {register } = AuthRepository();
+
+    const toast = useToast();
+
+    const registerUser = (user: User) => {
+        register(user).then(() => {
+            toast.add("User registered successfully", "success");
+        }).catch((error) => {
+            const message = error.response?.data as string || "Network error";
+            toast.add(message, "error");
+        })
+    }
+
+    return (
+        <div className="h-full w-full flex flex-col items-center justify-center">
+            <div
+                className="bg-white p-8 w-4/5 md:w-1/2 lg:w-1/3 flex flex-col items-center justify-center space-y-6 shadow-lg rounded-lg animate-fadeIn">
+                <div className="flex items-center justify-center p-4 w-full h-20 animate-fadeIn">
+                    <Image src="icons/user.png" alt="Note icon" width={50} height={50}/>
+                    <h1 className="text-5xl font-bold ml-4">Register</h1>
+                </div>
+                <UserForm mode="register" onSend={registerUser}/>
+                <Link href="/login"> Already have an account? Login </Link>
+            </div>
+        </div>
+
+    );
+}
