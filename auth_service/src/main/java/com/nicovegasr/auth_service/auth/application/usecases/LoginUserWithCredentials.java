eec643fc9ea9351b9955da08f1552623dbe1e6ad
nicovegasr <nicovegasr@gmail.com>
2024-06-24 18:54:02 +0100

    Change return of login endpoint

--- auth_service/src/main/java/com/nicovegasr/auth_service/auth/application/usecases/LoginUserWithCredentials.java
+++ auth_service/src/main/java/com/nicovegasr/auth_service/auth/application/usecases/LoginUserWithCredentials.java
@@ -3,11 +3,12 @@ package com.nicovegasr.auth_service.auth.application.usecases;
 import com.nicovegasr.auth_service.auth.application.exceptions.AuthenticationError;
 import com.nicovegasr.auth_service.auth.domain.models.User;
 import com.nicovegasr.auth_service.auth.domain.repositories.UserRepository;
+import com.nicovegasr.auth_service.auth.infrastructure.api.responses.LoginResponse;
 
 import java.util.Optional;
 
 public class LoginUserWithCredentials {
-    public static String login(UserRepository userRepository, String username, String password, String secretKey) {
+    public static LoginResponse login(UserRepository userRepository, String username, String password, String secretKey) {
          Optional<User> user = userRepository.findByUsername(username);
         if (user.isEmpty()) {
             throw new AuthenticationError("User: " + username + " not found");
@@ -15,6 +16,8 @@ public class LoginUserWithCredentials {
         if (!user.get().getPassword().getCredential().equals(password)) {
             throw new AuthenticationError("Passwords do not match");
         }
-        return JwtToken.create(username, password, secretKey);
+        String token = JwtToken.create(username, password, secretKey);
+        String email = user.get().getEmail().getValue();
+        return new LoginResponse(username, email, token);
     }
 }
