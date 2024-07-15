caee23a3e30a5b19603b3aa034d265814c33eda0
nicovegasr <nicovegasr@gmail.com>
2024-07-15 21:44:46 +0100

    sort imports

--- auth_service/src/main/java/com/nicovegasr/auth_service/auth/application/exceptions/AuthenticationError.java
+++ auth_service/src/main/java/com/nicovegasr/auth_service/auth/application/exceptions/AuthenticationError.java
@@ -1,7 +1,5 @@
 package com.nicovegasr.auth_service.auth.application.exceptions;
 
-import com.nicovegasr.auth_service.auth.domain.exceptions.UserException;
-
 public class AuthenticationError extends RuntimeException {
     public AuthenticationError(String message) {
         super(message);
